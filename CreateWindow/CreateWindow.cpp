d8501adebeb1361d0d40ffd1b2612cb7bc8561cd
Tkachuk Ivan <tkachyk2222@gmail.com>
2017-03-04 22:50:00 +0200

    fourth commit

--- CreateWindow/CreateWindow.cpp
+++ CreateWindow/CreateWindow.cpp
@@ -1,6 +1,5 @@
 // CreateWindow.cpp : Defines the entry point for the application.
 //
-
 #include "stdafx.h"
 #include<stdlib.h>
 #include "CreateWindow.h"
@@ -8,7 +7,12 @@
 #include<memory>
 #include<set>
 #include<array>
+#include<deque>
 #include"commctrl.h"
+#include<stdlib.h>	
+
+using std::array;
+using std::deque;
 using std::set;
 #define MAX_LOADSTRING 100
 #define WM_START WM_USER+1
@@ -20,17 +24,18 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 HWND cntOfButt;
-HWND ParentWindow;
 INT radioCheck = 1;
+HWND ParentWindow;
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
+INT					compare(CONST void *ap, CONST void *bp);
 INT					randomNumbInStr();
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    CountOfButton(HWND, UINT, WPARAM, LPARAM);
-void				searchClkdButton(LPARAM, set<HWND>, HWND);
-
+void				searchClkdButton(LPARAM lParam, set<HWND>hButt, HWND hDlg, INT cnt, deque<INT>randNum);
+BOOL				allIsClecked(LPARAM, set<HWND>, HWND);
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -62,6 +67,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	static deque<INT>randNumb;;
+	static INT cntClkBut = 0;
+	//::radioCheck = 1;
 	static HWND listOfNumers;
 	static HWND ProgBar;
 	static INT timer = 0;
@@ -75,6 +83,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 		
 		case WM_INITDIALOG: {
+			
 			listOfNumers = GetDlgItem(hDlg, IDC_LIST1);
 															//progress bar
 			TCHAR buffOfMin[6];
@@ -83,7 +92,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 			SendMessage(ProgBar, PBM_SETRANGE, 0, MAKELPARAM(0, tmp));
 			SendMessage(ProgBar, PBM_SETPOS, 0, 0);
 
-
+			
 			SendMessage(ProgBar, PBM_SETBARCOLOR, 0, RGB(0, 255, 0));
 			ProgBar = GetDlgItem(hDlg, IDC_PROGRESS1);
 			return (INT_PTR)TRUE; 
@@ -91,11 +100,14 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 
 
 		case WM_START:{
+			for (INT i = 0; i < 100; i++)
+				randNumb.push_back(i);
+			std::random_shuffle(randNumb.begin(), randNumb.end());
 
-			//MessageBox(hDlg, L"Yes", L"yees", MB_OK);
 			if (radioCheck == 1) {
+				nWidth = 70; nHeight = 70;
 				for (int i = 1; i < 10; ++i) {
-					INT num = randomNumbInStr();
+					INT num = randNumb[i];
 					TCHAR buf[5];
 					_stprintf_s(buf, _T("%d"), num);
 					if (i == 1)
@@ -106,18 +118,16 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 					}
 					else
 						x += 80;
-		//			hBut.push_back((CreateWindow(L"BUTTON", buf, WS_CHILD | WS_VISIBLE | WS_TABSTOP, x, y, nWidth, nHeight, hDlg, NULL, hInst, NULL)));
 					hButt.insert(CreateWindow(L"BUTTON", buf, WS_CHILD | WS_VISIBLE | WS_TABSTOP, x, y, nWidth, nHeight, hDlg, NULL, hInst, NULL));
 				}
 			}
 
-
 			else if (radioCheck == 2) {
-				INT num = randomNumbInStr();
-				TCHAR buf[5];
-				_stprintf_s(buf, _T("%d"), num);
 				nWidth = 50; nHeight = 50;
 				for (int i = 1; i < 17; ++i) {
+					INT num = randNumb[i];
+					TCHAR buf[5];
+					_stprintf_s(buf, _T("%d"), num);
 					if (i == 1)
 						x = 10;
 					else if (i == 5 || i == 9||i==13) {					
@@ -127,16 +137,13 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 					else
 						x += 60;
 					hButt.insert(CreateWindow(L"BUTTON", buf, WS_CHILD | WS_VISIBLE | WS_TABSTOP, x, y, nWidth, nHeight, hDlg, NULL, hInst, NULL));
-
-		//			hBut.push_back((CreateWindow(L"BUTTON", buf, WS_CHILD | WS_VISIBLE | WS_TABSTOP, x, y, nWidth, nHeight, hDlg, NULL, hInst, NULL)));
 				}
 			}
-
 
 			else if (radioCheck == 3) {
 				nWidth = 40; nHeight = 40;
 				for (int i = 1; i < 26; ++i) {
-					INT num = randomNumbInStr();
+					INT num = randNumb[i];
 					TCHAR buf[5];
 					_stprintf_s(buf, _T("%d"), num);
 					if (i == 1)
@@ -148,12 +155,11 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 					else
 						x += 50;
 					hButt.insert(CreateWindow(L"BUTTON", buf, WS_CHILD | WS_VISIBLE | WS_TABSTOP, x, y, nWidth, nHeight, hDlg, NULL, hInst, NULL));
-
-		//			hBut.push_back((CreateWindow(L"BUTTON", buf, WS_CHILD | WS_VISIBLE | WS_TABSTOP, x, y, nWidth, nHeight, hDlg, NULL, hInst, NULL)));
-				
 				}
 			}
+
 			id = SetTimer(hDlg, TIMERID, 1000, NULL);
+
 			return (INT_PTR)TRUE;
 		}
 
@@ -197,7 +203,7 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 							  
 			}
 			
-			searchClkdButton(lParam, hButt, hDlg);
+			searchClkdButton(lParam, hButt, hDlg, 16, randNumb);
 			
 			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
 			{
@@ -215,7 +221,7 @@ INT_PTR CALLBACK    CountOfButton(HWND hDlg, UINT message, WPARAM wParam, LPARAM
 	switch (message)
 	{
 	case WM_INITDIALOG: {
-		radioCheck = 1;
+		//radioCheck = 0;
 		CheckRadioButton(hDlg, IDC_RADIO1, IDC_RADIO3, IDC_RADIO1);
 		return (INT_PTR)TRUE;
 	}
@@ -268,16 +274,58 @@ INT	randomNumbInStr() {
 	return{ rand() % 100 + 1 };
 }
 
-void searchClkdButton(LPARAM lParam, set<HWND>hButt, HWND hDlg) {
-	//search clicked button
+void searchClkdButton(LPARAM lParam, set<HWND>hButt, HWND hDlg, INT cnt, deque<INT>randNum) {
+											//search clicked button
+	//const INT cnt2 = 10;
+	//INT *sz = const_cast<INT*>(&cnt2);
+	//*sz = cnt;
+	//deque<INT>sorted(randNum);
+	//array<INT, cnt2>arr;
+//	for (int i = 0; i < *sz; i++)
+//	{
+//		arr[i] = randNum[i];
+//}
+//	std::sort(sorted.begin(), sorted.end());
 	auto it = hButt.find((HWND)(lParam));
 	if (it != hButt.end())
 		if (*it == (HWND)lParam) {
+//			auto iter = sorted.begin();
+			
+
 			//MessageBox(hDlg, L"It's work =)", L"work =)", MB_OK);
 			TCHAR buffOfWinText[6];
 			GetWindowText(*it, buffOfWinText, 6);
-			//and add text to listbox
-			SendMessage(GetDlgItem(hDlg, IDC_LIST1), LB_ADDSTRING, 0, (LPARAM)buffOfWinText);
-			EnableWindow(*it, FALSE);
+//			if ((_wtoi(buffOfWinText)) == *iter) {
+				SendMessage(GetDlgItem(hDlg, IDC_LIST1), LB_ADDSTRING, 0, (LPARAM)buffOfWinText);//and add text to listbox
+				EnableWindow(*it, FALSE);
+//				sorted.pop_front();
+//			}
 		}
+	/*::cntClkBut++;
+	if (radioCheck == 1) {
+		if (cntClkBut == 9)
+			MessageBox(hDlg, L"Congratulation!! You are WIN", L"Game Over", IDOK);
+	}
+	else if (radioCheck == 2) {
+		if (cntClkBut == 16)
+			MessageBox(hDlg, L"Congratulation!! You are WIN", L"Game Over", IDOK);
+	}
+	else if (radioCheck == 25) {
+		MessageBox(hDlg, L"Congratulation!! You are WIN", L"Game Over", IDOK);
+	}*/
 }
+INT compare(CONST void *ap, CONST void *bp)
+{
+	CONST INT *a = (INT *)ap;
+	CONST INT *b = (INT *)bp;
+	if (*a < *b)
+		return -1;
+	else if (*a > *b)
+		return 1;
+	else
+		return 0;
+}
+//BOOL allIsClecked(LPARAM lParam, set<HWND>hButt, HWND hDlg) {
+//	INT cnt = 0;
+//	std::for_each(hButt.begin(), hButt.end(), []() {if()})
+//}
